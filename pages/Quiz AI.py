c44e079308dd11e1361ed443190b14e617c9dd87
MJ <mwjang628@gmail.com>
2024-07-20 21:18:37 +1000

    update quiz AI

--- pages/Quiz AI.py
+++ pages/Quiz AI.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
 
 
 st.set_page_config(
@@ -7,6 +9,23 @@ st.set_page_config(
     page_icon="💯",
 )
 st.title("💯 Quiz AI")
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
 
 with st.sidebar:
     choice = st.selectbox(
@@ -21,10 +40,13 @@ with st.sidebar:
             "Upload a .txt .pdf or .docx file",
             type=["pdf", "txt", "docx"],
         )
+        if file:
+            docs = split_file(file)
+        docs
     else:
         topic = st.text_input("What do you want to search?")
         if topic:
-            retriever = WikipediaRetriever(top_k_results=5)
+            retriever = WikipediaRetriever(top_k_results=2)
             with st.status("Searching Wikipedia..."):
                 docs = retriever.get_relevant_documents(topic)
             st.write(docs)
