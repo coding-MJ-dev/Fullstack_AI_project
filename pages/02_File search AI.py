7d6d16d60b064e923f318471656570556881522c
MJ <mwjang628@gmail.com>
2024-07-30 22:02:23 +1000

    Add memory storage

--- pages/02_File search AI.py
+++ pages/02_File search AI.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
@@ -9,6 +9,8 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.memory import ConversationSummaryBufferMemory
+from langchain.storage import LocalFileStore
+
 
 st.set_page_config(
     page_title="File search AI",
@@ -50,25 +52,37 @@ llm = ChatOpenAI(
     ],
 )
 
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit=120,
+    memory_key="chat_history",
+    return_messages=True,
+)
+
 
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
+
     with open(file_path, "wb") as f:
         f.write(file_content)
+
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
+
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
+
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
+
     return retriever
 
 
@@ -96,16 +110,31 @@ def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
 
+def save_memory(input, output):
+    st.session_state["chat_history"].append({"input": input, "output": output})
+
+
+def load_memory(input):
+    return memory.load_memory_variables({})["chat_history"]
+
+
+def invoke_chain(message):
+    result = chain.invoke(message)
+    save_memory(message, result.content)
+    print(result)
+
+
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
             """
-            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
-            
+
+            Answer the question using ONLY the following context and not your trading data. If you don't know the answer just say you don't know. DON'T make anything up.            
             Context: {context}
             """,
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         ("human", "{question}"),
     ]
 )
@@ -131,14 +160,17 @@ if file:
         chain = (
             {
                 "context": retriever | RunnableLambda(format_docs),
+                "chat_history": load_memory,
                 "question": RunnablePassthrough(),
             }
             | prompt
             | llm
         )
         with st.chat_message("ai"):
-            chain.invoke(message)
+            invoke_chain(message)
+            memory.load_memory_variables({})
 
 
 else:
     st.session_state["messages"] = []
+    st.session_state["chat_history"] = []
