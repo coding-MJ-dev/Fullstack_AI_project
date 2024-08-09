80c32c3fbf7084a364d9eeed4f03c465acbae8c7
MJ <mwjang628@gmail.com>
2024-08-09 13:42:24 +1000

    Extracting Audio

--- pages/02_Video AI.py
+++ pages/02_Video AI.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import subprocess
+
+
+def extract_audio_from_video(video_path):
+    audio_path = video_path.replace("mp4", "mp3")
+    command = [
+        "ffmpeg",
+        # input
+        "-i",
+        video_path,
+        # ignore the vedio => vedio nope => vn
+        "-vn",
+        audio_path,
+    ]
+    subprocess.run(command)
+
 
 st.set_page_config(
     page_title="Video AI",
@@ -24,3 +40,12 @@ Get started by uploading a video file in the sidebar.
 
 with st.sidebar:
     video = st.file_uploader("Video", type=["mp4", "avi", "mkv", "mov"])
+    if video:
+        video_content = video.read()
+        video_path = f"./.cache/{video.name}"
+        # wb > write in binary
+        with open(video_path, "wb") as f:
+            # save the video that user upload to cache file "f"
+            f.write(video_content)
+
+        extract_audio_from_video(video_path)
