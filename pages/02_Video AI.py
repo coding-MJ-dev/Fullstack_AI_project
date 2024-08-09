98663f63aee4288de6c795e79c9afc9384cf9d69
MJ <mwjang628@gmail.com>
2024-08-09 14:36:15 +1000

    Transcribing audio

--- pages/02_Video AI.py
+++ pages/02_Video AI.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import subprocess
 import math
+import glob
+import openai
 from pydub import AudioSegment
 
 
@@ -33,6 +35,23 @@ def cut_audio_in_chunks(audio_path, chunk_size, chunks_folder):
         chunk.export(f"{chunks_folder}/chunk_{i}.mp3", format="mp3")
 
 
+def transcribe_chunks(chunk_folder, destination):
+    files = glob.glob(f"{chunk_folder}/*.mp3")
+    files.sort()
+    # find mp3 files in the folder
+    for file in files:
+        # get transcript // append transcribe to destination
+        with open(file, "rb") as audio_file, open(destination, "a") as text_file:
+            transcript = openai.Audio.transcribe(
+                "whisper-1",
+                audio_file,
+            )
+            # add it to the destination
+            text_file.write(transcript["text"])
+
+
+###-------- page start ----------###
+
 st.set_page_config(
     page_title="Video AI",
     page_icon="🎬",
@@ -60,10 +79,13 @@ with st.sidebar:
 
 
 if video:
+    chunks_folder = "./.cache/chunks"
+
     with st.status("Loading video..."):
         video_content = video.read()
         video_path = f"./.cache/{video.name}"
         audio_path = video_path.replace("mp4", "mp3")
+        transcript_path = video_path.replace("mp4", "txt")
         # wb > write in binary
         with open(video_path, "wb") as f:
             # save the video that user upload to cache file "f"
@@ -71,4 +93,6 @@ if video:
     with st.status("Extracting audio..."):
         extract_audio_from_video(video_path)
     with st.status("cutting audio segments..."):
-        cut_audio_in_chunks(audio_path, 10, "./.cache/chunks")
+        cut_audio_in_chunks(audio_path, 10, chunks_folder)
+    with st.status("Transcribing audio..."):
+        transcribe_chunks(chunks_folder, transcript_path)
