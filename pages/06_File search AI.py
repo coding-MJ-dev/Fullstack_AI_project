c016c0aba8c1fde3beb872cfed19450656f15d39
MJ <mwjang628@gmail.com>
2024-08-12 14:26:02 +1000

    fix bugs

--- pages/06_File search AI.py
+++ pages/06_File search AI.py
@@ -1,32 +1,17 @@
-import streamlit as st
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.embeddings import CacheBackedEmbeddings, OllamaEmbeddings
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOllama
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.storage import LocalFileStore
-
+import streamlit as st
 
 st.set_page_config(
-    page_title="File search AI",
-    page_icon="🗒️",
-)
-st.title("🗒️ File search AI")
-st.markdown(
-    """
-Welcome!
-            
-Use this chatbot to ask questions about your documentation!
-
-It will answer based on the content inside the files.
-
-Please upload your file on the sidebar!
-"""
+    page_title="PrivateGPT",
+    page_icon="🔒",
 )
 
 
@@ -44,7 +29,8 @@ class ChatCallbackHandler(BaseCallbackHandler):
         self.message_box.markdown(self.message)
 
 
-llm = ChatOpenAI(
+llm = ChatOllama(
+    model="mistral:latest",
     temperature=0.1,
     streaming=True,
     callbacks=[
@@ -52,37 +38,25 @@ llm = ChatOpenAI(
     ],
 )
 
-memory = ConversationSummaryBufferMemory(
-    llm=llm,
-    max_token_limit=120,
-    memory_key="chat_history",
-    return_messages=True,
-)
-
 
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
-
+    file_path = f"./.cache/private_files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
-
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
-
+    embeddings = OllamaEmbeddings(model="mistral:latest")
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
-
     return retriever
 
 
@@ -110,35 +84,26 @@ def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
 
-def save_memory(input, output):
-    st.session_state["chat_history"].append({"input": input, "output": output})
-
-
-def load_memory(input):
-    return memory.load_memory_variables({})["chat_history"]
-
-
-def invoke_chain(message):
-    result = chain.invoke(message)
-    save_memory(message, result.content)
-    print(result)
-
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-
-            Answer the question using ONLY the following context and not your trading data. If you don't know the answer just say you don't know. DON'T make anything up.            
-            Context: {context}
-            """,
-        ),
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("human", "{question}"),
-    ]
+prompt = ChatPromptTemplate.from_template(
+    """Answer the question using ONLY the following context and not your training data. If you don't know the answer just say you don't know. DON'T make anything up.
+    
+    Context: {context}
+    Question:{question}
+    """
 )
 
+
+st.title("PrivateGPT")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
+"""
+)
 
 with st.sidebar:
     file = st.file_uploader(
@@ -148,11 +113,7 @@ with st.sidebar:
 
 if file:
     retriever = embed_file(file)
-    send_message(
-        "Hi, I'm ready to answer! please ask anything about your file.",
-        "ai",
-        save=False,
-    )
+    send_message("I'm ready! Ask away!", "ai", save=False)
     paint_history()
     message = st.chat_input("Ask anything about your file...")
     if message:
@@ -160,17 +121,14 @@ if file:
         chain = (
             {
                 "context": retriever | RunnableLambda(format_docs),
-                "chat_history": load_memory,
                 "question": RunnablePassthrough(),
             }
             | prompt
             | llm
         )
         with st.chat_message("ai"):
-            invoke_chain(message)
-            memory.load_memory_variables({})
+            chain.invoke(message)
 
 
 else:
     st.session_state["messages"] = []
-    st.session_state["chat_history"] = []
